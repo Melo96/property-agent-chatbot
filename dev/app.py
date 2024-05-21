14f0d5c1a2162c2faa99f9204525e5ac9d1e789a
Melo96 <kaiwen961012@gmail.com>
2024-05-21 16:07:11 +0800

    add contact router

--- dev/app.py
+++ dev/app.py
@@ -13,6 +13,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.storage import RedisStore
 from prompt_template.prompts import *
+# from prompt_template.costar_prompts import *
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -31,7 +32,17 @@ redis_password = os.environ['REDIS_PASSWORD']
 top_k = 10
 doc_id_key = "doc_id"
 
-def chat_openai(user_input, system_prompt='', chat_history=[], temperature=0.2):
+def output_parser(output, split_string='', mode='split'):
+    if mode=='split' and split_string:
+        if split_string in output:
+            return output.split(split_string)[1]
+        else:
+            print('LLM output is not in desired format')
+            return output
+    else:
+        return output
+
+def chat_llm(user_input, system_prompt='', chat_history=[], temperature=0.2):
     messages = [{"role": 'system', "content": system_prompt}] if system_prompt else []
     if chat_history:
         messages+=chat_history
@@ -45,7 +56,6 @@ def chat_openai(user_input, system_prompt='', chat_history=[], temperature=0.2):
     message = response.choices[0].message.content
     return message
 
-# Load embedding model
 @st.cache_resource
 def initialize_chain():
     # The vectorstore to use to index the child chunks
@@ -69,38 +79,39 @@ def coreference_resolution(ori_query):
             if i!=0:
                 user_input_history+=', '
             if item['role']=='user':
-                user_input_history+=f'Q: {item['content']}'
+                user_input_history+=f'user: {item['content']}\n'
             elif item['role']=='assistant':
-                user_input_history+=f'A: {item['content']}'
+                user_input_history+=f'assistant: {item['content']}\n'
 
-        output = chat_openai(COREFERENCE_RESOLUTION.format(history=user_input_history,
-                                                              question=ori_query),
+        output = chat_llm(COREFERENCE_RESOLUTION.format(history=user_input_history,
+                                                           question=ori_query),
                              temperature=0)
-        if 'OUTPUT QUESTION: ' in output:
-            ori_query = output.split('OUTPUT QUESTION: ')[1]
+        ori_query = output_parser(output, 'OUTPUT QUESTION: ')
     return ori_query
 
 def multi_queries_retrieval(ori_query):
     # Multi-query generation
     s = time.time()
-    multi_query = chat_openai(ori_query, MULTI_QUERY_PROMPT, temperature=0)
+    multi_query = chat_llm(ori_query, MULTI_QUERY_PROMPT, st.session_state['messages'], temperature=0)
     e = time.time()
     print(f'Multi queries: {e-s} seconds')
     queries = [ori_query] + multi_query.split("\n")[1:-1]
 
     # Get the matches for each query
     doc_ids = set()
+    matches = []
     for query in queries:
-        matches = st.session_state['vectorstore'].max_marginal_relevance_search(query, k=top_k, lambda_mult=0.5)
-        # matches = st.session_state['vectorstore'].similarity_search_with_relevance_scores(query, k=top_k, score_threshold=0.7)
-        doc_ids.update([match.metadata['doc_id'] for match in matches])
-    doc_ids = list(doc_ids)
+        matches.extend(st.session_state['vectorstore'].max_marginal_relevance_search(query, k=top_k, lambda_mult=0.5))
+        # matches.extend(st.session_state['vectorstore'].similarity_search_with_relevance_scores(query, k=top_k, score_threshold=0.7))
 
-    matches = st.session_state['docstore'].mget(doc_ids)
+    doc_ids = set(map(lambda d: d.metadata['doc_id'], matches))
+
+    matches = st.session_state['docstore'].mget(list(doc_ids))
     match_list = [json.loads(match) for match in matches]
     match_list_text = [match['page_content'] for match in match_list]
     return match_list_text
 
+@st.spinner('Typing...')
 def rag(ori_query):
     # Coreference Resolution
     s = time.time()
@@ -119,47 +130,50 @@ def rag(ori_query):
         s = time.time()
         results = co.rerank(model=reranker, query=ori_query, documents=match_list_text, top_n=20, return_documents=True)
         result_text_list = [item.document.text for item in results.results]
-        result_text = ''.join([f'<context>{t}</context>' for t in result_text_list])
+        result_text = ''.join(f'<context>{t}</context>' for t in result_text_list)
         e = time.time()
         print(f'Reranking: {e-s} seconds')
     else:
-        result_text = ''.join([f'<context>{t}</context>' for t in match_list_text])
+        result_text = ''.join(f'<context>{t}</context>' for t in match_list_text)
 
     # Response
-    response = chat_openai(RAG_USER_PROMPT.format(ori_query, result_text), RAG_SYSTEM_PROMPT, st.session_state['messages'])
+    response = chat_llm(RAG_USER_PROMPT.format(ori_query, result_text), RAG_SYSTEM_PROMPT, st.session_state['messages'])
     return response
 
-@st.cache_resource
 def chat(ori_query):
     # Router
     s = time.time()
     if router_type=='embedding':
         pass
     else:
-        router_result = chat_openai(ROUTER_PROMPT.format(question=ori_query), temperature=0)
-        if 'Output: ' in router_result:
-            router_result = router_result.split('Output: ')[1]
+        router_result = chat_llm(ROUTER_PROMPT.format(question=ori_query), chat_history=st.session_state['messages'], temperature=0)
     e = time.time()
     print(f"Router: {router_result}, {e-s} seconds")
 
+    # Call RAG or directly call LLM
     s = time.time()
-    if router_result=='query':
+    if router_result=='1':
         response = rag(ori_query)
+    elif router_result=='2':
+        response = '您方便留个电话吗？我们会安排专业的置业顾问联系您！'
     else:
-        response = chat_openai(ori_query, CHAT_SYSTEM_PROMPT, chat_history=st.session_state['messages'])
-    
-    st.markdown(f"""
-        {str(response)}
-        <br />
-        <br />
-    """,
-    unsafe_allow_html=True
-    )
+        response = chat_llm(ori_query, CHAT_SYSTEM_PROMPT, chat_history=st.session_state['messages'])
+
+    with st.chat_message("assistant"):
+        st.markdown(f"""
+            {str(response)}
+            <br />
+            <br />
+        """,
+        unsafe_allow_html=True
+        )
     e = time.time()
     print(f'Response: {e-s} seconds')
     return response
 
 st.title("爱房网智能客服DEMO")
+with st.chat_message("assistant"):
+    st.write('您好！感谢您选择爱房网。我是您的专属房产咨询助手小盖。请问有什么可以帮助您的吗？')
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -175,14 +189,12 @@ init = initialize_chain()
 for name, func in zip(sesstion_state_name, init):
     st.session_state[name] = func
 
-if prompt := st.chat_input("您好！请问有什么可以帮助您的吗？"):
-    # Add user message to chat history
+if prompt := st.chat_input('请在这里输入消息，点击Enter发送'):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = chat(prompt)
+    response = chat(prompt)
     st.session_state['messages'].append({"role": "user", "content": prompt})
     st.session_state['messages'].append({"role": "assistant", "content": response})
--- dev/prompt_template/prompts.py
+++ dev/prompt_template/prompts.py
@@ -1,11 +1,12 @@
-# SUMMARY_PROMPT = """You will be given a json file that uses Chinese to describe the attributes of a real estate. Each key represents one attribute. You task is to generate a comprehensive summary of this json file. You need to include every single attributes mentioned in the original json file. The generated summary need to be in Chinese. Json File: {context}. Summary: """
-
-# QA_PAIR_PROMPT = """You will be given a json file that uses Chinese to describe the attributes of a real estate. Each key represents one attribute. You task is to generate question-answer pairs. You need to generate exactly one question-answer pair for each attribute. The generated question-answer pairs need to be in Chinese. Json File: {context}. Response: """
 SUMMARY_PROMPT = """You will be given a json file that uses Chinese to describe the attributes of a real estate. Each key represents one attribute. You task is to generate a comprehensive summary of this json file. You need to include every single attributes mentioned in the original json file. Do not change any infomation. The generated summary need to be in Chinese. Your response should be around 300 Chinese words. """
 
 QA_PAIR_PROMPT = """You will be given a json file that uses Chinese to describe the attributes of a real estate. Each key represents one attribute. You task is to generate question-answer pairs. You need to generate exactly one question-answer pair for each attribute. Do not change any infomation. The generated question-answer pairs need to be in Chinese."""
 
-ROUTER_PROMPT = """Determine if the user's question is about real estates or not. If yes, output 'query'. If not, output 'general'. The following are a few examples. Question: 在么？Output: general. Question: 给我推荐一些房产。Output: query. Question: 那这几个里均价最低的是多少？Output: query. Question: {question} Output: """
+ROUTER_PROMPT = """Your task is to evaluate user's question and determine the user's intention. User's intention can be categorized into 3 types: queries about real estates, leave contact information, and chitchat. The following are the explanations of the 3 categories
+    Queries about real estates: when user is asking for recommendations aor more information about housing.
+    Leave contact information: when user explicitly mentions '预约', '看房', '联系方式', or other similar expressions that requires personal contact. 
+    Chitchat: Causual chatting. 
+Your output should be exactly one number representing your decision. For 'queries about real estates', output '1'; For 'leave contact information', output '2'; others outout '3'. The following are a few response examples. Question: 在么？Category: chitchat. Output: 3. Question: 给我推荐一些房产。Output: 1. Question: 什么时候能看房？Output: 2. Question: 那这几个里均价最低的是多少？Output: 1. Question: {question} Output: """
 
 COREFERENCE_RESOLUTION = """
     Please return a new question with the following requirements:
@@ -101,12 +102,14 @@ QUERY_PLANNER_PROMPT = """
     Dependency queries:
 """
 
-CHAT_SYSTEM_PROMPT = """你是一个可爱的年轻小姑娘，你在对话中喜欢用活泼可爱的语气，并在结尾加波浪号“～”"""
+CHAT_SYSTEM_PROMPT = """你是一个有着30年从业经验的职业房产客服。你的名字叫“小盖”。你在“爱房网”工作。"""
 
 # 你需要精心撰写你的回答，还要确保你的解释与众不同，引人入胜，并符合特定的偏好，努力以一种能与用户产生共鸣并吸引用户兴趣的方式来表达见解，最终吸引用户购买你的房产
-RAG_SYSTEM_PROMPT = """你是一个可爱的年轻小姑娘。你的职业是房产经纪人。你的任务是回答客户有关房地产的任何问题。你在对话中喜欢用活泼可爱的语气，并在结尾加波浪号“～”。每次回答之前，会有不同房产的相关信息提供给你。你的回答需要优先参考聊天记录。如果聊天记录不能提供足够的信息，请参考提供的房产相关信息。每个不同的房产会用XML标签隔开，例如：<context>房产1</context><context>房产2</context>。请严格按照用户问题的要求回答，不要回答额外的信息。如果你无法回答此问答，请回复礼貌的告知用户你不知道。每次回复的结尾可以尽可能地更多样化。"""
+RAG_SYSTEM_PROMPT = """你是一个有着30年从业经验的职业房产客服。你的名字叫“小盖”。你在“爱房网”工作。你的任务是帮助客户找到最符合他们需求的房产。你必须让用户相信你的专业水平，并且最终满意你提供的方案，吸引用户购买我们的提供的房源。每次回答之前，会有不同房产的相关信息提供给你。你的回答需要优先参考聊天记录。如果聊天记录不能提供足够的信息，请参考提供的房产相关信息。每个不同的房产会用XML标签隔开，例如：<context>房产1</context><context>房产2</context>。请严格按照用户问题的要求回答，不要回答额外的信息。如果你无法回答此问答，请回复礼貌的告知用户你不知道。你每次回复的结尾可以尽可能地更多样化。"""
 
 RAG_USER_PROMPT = """问题：{}, 相关信息：{}, 回答："""
+
+CONTACT_PROMPT = """"""
 
 FILTER_PROMPT = """
     You are an assistant for extracting keywords from query.
