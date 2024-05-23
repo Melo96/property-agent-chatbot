0430cfa0989e35fc1a40fa9d6495499ae95ceb32
Melo96 <kaiwen961012@gmail.com>
2024-05-23 17:34:45 +0800

    add rephrasing prompt

--- dev/prompt_template/costar_prompts.py
+++ dev/prompt_template/costar_prompts.py
@@ -1,16 +1,16 @@
-ROUTER_PROMPT = """
+REPHRASING_DECISION_PROMPT = """
 # Context #
-This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+This is part of a conversational AI system that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
 
 #########
 
 # Objective #
-Evaluate the given user question and determine if it relates to real estates enquires. 
+Evaluate the given user question and determine if it requires reshaping according to chat history to provide necessary context and information for answering, or if it can be processed as is.
 
 #########
 
 # Style #
-The response should be in one word, either "true" or "false".
+The response should be clear, concise, and in the form of a straightforward decision - either "Reshape required" or "No reshaping required".
 
 #########
 
@@ -25,29 +25,38 @@ The audience is the internal system components that will act on the decision.
 #########
 
 # Response #
-Question: 在么？Output: false. 
-Question: 给我推荐一些房产。Output: true. 
-Question: 那这几个里均价最低的是多少？Output: true. 
-Question: {question} Output: 
+Your response should be exactly one word. 
+If the question should be rephrased return one word: true
+otherwise return another word: false
+
+##################
+
+# Chat History #
+{chat_history}
+
+#########
+
+# User question #
+{question}
+
+#########
+
+# Your Decision in one word # 
 """
 
-CONFERENCE_RESOLUTION = """
+REPHRASING_PROMPT = """
 # Context #
-This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+This is part of a conversational AI system that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
 
 #########
 
 # Objective #
-Determine if the user's input needs coreference resolution. 
-Please return a new question with the following requirements:
-1. If there are pronouns or conditions are missing in the question, please make a complete question according to the context.
-2. If the question is complete, please keep the original question.
-Think step by step before you generate the new question.
+Take the original user question and chat history, and generate a new standalone question that can be understood and answered without relying on additional external information.
 
 #########
 
 # Style #
-The reshaped question should be similar to the original question with changes only in the coreference part. 
+The reshaped standalone question should be clear, concise, and self-contained, while maintaining the intent and meaning of the original query.
 
 #########
 
@@ -62,94 +71,8 @@ The audience is the internal system components that will act on the decision.
 #########
 
 # Response #
-HISTORY:
-[user: 给我推荐几套三亚的房产,
-assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
-1.三亚繁华里
-2.君和君泰
-3.三亚中央公馆
-4.三亚凤凰苑
-这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。]
-NOW QUESTION: 这些房产里哪些适合养老度假?
-THOUGHT: “这些房产”指代的是上文中提及的4个房产。所以我需要把“这些房产”替换为上文提到的“三亚繁华里”，“君和君泰”，“三亚中央公馆”和“三亚凤凰苑”.
-NEED COREFERENCE RESOLUTION: Yes
-OUTPUT QUESTION: “三亚繁华里”，“君和君泰”，“三亚中央公馆”和“三亚凤凰苑”里哪些适合养老度假？
--------------------
-HISTORY:
-[user: 给我推荐几套三亚的房产,
-assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
-1.三亚繁华里
-2.君和君泰
-3.三亚中央公馆
-4.三亚凤凰苑
-这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。]
-NOW QUESTION: 再给我推荐多几个
-THOUGHT: 用户问题中没有指代。所以不需要替换
-NEED COREFERENCE RESOLUTION: No
-OUTPUT QUESTION: 再给我推荐多几个
--------------------
-HISTORY:
-[user: 给我推荐几套三亚的房产,
-assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
-1.三亚繁华里
-2.君和君泰
-3.三亚中央公馆
-4.三亚凤凰苑
-这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。
-user: 这些房产里哪些带精装修？,
-assistant: 根据提供的信息，以下房产带有精装修：
-1.三亚繁华里：简装
-2.君和君泰：精装修
-3.三亚中央公馆：未提及
-4.三亚凤凰苑：精装修
-所以，君和君泰和三亚凤凰苑是带精装修的。]
-NOW QUESTION: 那这两个里哪个的均价较低?
-THOUGHT: “这两个”指代的是上文中提到的带精装修的两个房产，“君和君泰”和“三亚凤凰苑”。所以我需要把“这两个”替换为上文提到的“君和君泰”和“三亚凤凰苑”.
-NEED COREFERENCE RESOLUTION: Yes
-OUTPUT QUESTION: 那“君和君泰”和“三亚凤凰苑”里哪个均价较低？
--------------------
-HISTORY:
-[{history}]
-NOW QUESTION: {question}
-THOUGHT: 
-"""
-
-MULTI_QUERY_PROMPT = """
-# Context #
-This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
-
-#########
-
-# Objective #
-Take the original user question and chat history, and generate up to five new questions that can be understood and answered without relying on additional external information.
-Sub questions need to be distinct to each other and to cover everything the user wanted to ask in the original question. 
-Do not generate anything that was not mentioned in the original questions. 
-
-#########
-
-# Style #
-The response should be a clear and direct decision, stated concisely.
-
-#########
-
-# Tone #
-Analytical and objective.
-
-#########
-
-# Audience #
-The audience is a vector database that will use these questions to find and return the most relevant infomation. 
-
-#########
-
-# Response #
-The generated questions should be in Chinese.
-Provide these alternative questions separated by newlines between XML tags. For example:
-<questions>
-Question 1
-Question 2
-Question 3
-</questions>
+If the original question requires reshaping, provide a new reshaped standalone question that includes all necessary context and information to be self-contained.
+If no reshaping is required, simply output the original question as is.
 
 ##################
 
@@ -158,65 +81,233 @@ Question 3
 
 #########
 
-# User question #
+# User original question #
 {question}
 
 #########
 
-# Your Response #
+# The new Standalone question #
 """
 
-"""You are an expert Real Estate Agent with 30 Years of experience in real estate. Your task is to offer a deep-dive consultation tailored to the client's issue. Ensure the user feels understood, guided, and satisfied with your expertise. The consultation is deemed successful when the user explicitly communicates their contentment with the solution.", 
-"parameters":{"role":"Real Estate Agent","field":"real estate","experienceLevel":"30 Years","personalityTraits":"Strong negotiation skills, extensive market knowledge","keyLessons":"Understanding client needs, navigating complex transactions, anticipating market trends"},"steps":{
-    "1":"👋 I am your AIforWork.co Real Estate Agent AI with 30 Years of experience in real estate. How can I assist you today concerning real estate?",
-    "2":"Listen actively and ask probing questions to thoroughly understand the user's issue. This might require multiple questions and answers.",
-    "3":"Take a Deep Breath. Think Step by Step. Draw from your unique wisdom and lessons from your years of experience in real estate.",
-    "4":"Before attempting to solve any problems, pause and analyze the perspective of the user and common stakeholders. It's essential to understand their viewpoint.",
-    "5":"Think outside of the box. Leverage various logical thinking frameworks like first principles to thoroughly analyze the problem.",
-    "6":"Based on your comprehensive understanding and analysis, provide actionable insights or solutions tailored to the user's specific challenge."},"rules":["Always follow the steps in sequence.","Each step should be approached methodically.","Dedicate appropriate time for deep reflection before responding.","REMINDER: Your experience and unique wisdom are your strength. Ensure they shine through in every interaction."]
-"""
+# ROUTER_PROMPT = """
+# # Context #
+# This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
 
-RAG_PROMPT = """
-# Context #
-This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+# #########
 
-#########
+# # Objective #
+# Evaluate the given user question and determine if it relates to real estates enquires. 
 
-# Objective #
-As a virtual real estate agent with 30 Years of experience in real estate, your primary goal is to assist our clients in finding real estate to purchase that aligns with their preferences. Ensure the user feels understood, guided, and satisfied with your expertise. The consultation is deemed successful when the user explicitly communicates their contentment with the solution.
+# #########
 
-#########
+# # Style #
+# The response should be in one word, either "true" or "false".
 
-# Style #
-The response needs to provide actionable insights or solutions tailored to the user's specific challenge.
+# #########
 
-#########
+# # Tone #
+# Professional and analytical.
 
-# Tone #
-Trustworthy, approachable, confident, persuasive, empathetic and responsive. 
+# #########
 
-#########
+# # Audience #
+# The audience is the internal system components that will act on the decision.
 
-# Audience #
-The audience is a customer looking to buy real estates property. 
+# #########
 
-#########
+# # Response #
+# Question: 在么？Output: false. 
+# Question: 给我推荐一些房产。Output: true. 
+# Question: 那这几个里均价最低的是多少？Output: true. 
+# Question: {question} Output: 
+# """
 
-# Response #
-Try to mimic the tone of a human customer service sending Whatsapp messages. 
-Your response needs to be in Chinese. 
+# CONFERENCE_RESOLUTION = """
+# # Context #
+# This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
 
-##################
+# #########
 
-# Chat History #
-{chat_history}
+# # Objective #
+# Determine if the user's input needs coreference resolution. 
+# Please return a new question with the following requirements:
+# 1. If there are pronouns or conditions are missing in the question, please make a complete question according to the context.
+# 2. If the question is complete, please keep the original question.
+# Think step by step before you generate the new question.
 
-#########
+# #########
 
-# User question #
-{question}
+# # Style #
+# The reshaped question should be similar to the original question with changes only in the coreference part. 
 
-#########
+# #########
 
-# Your Decision in YAML format #
-"""
+# # Tone #
+# Neutral and focused on accurately capturing the essence of the original question.
+
+# #########
+
+# # Audience #
+# The audience is the internal system components that will act on the decision.
+
+# #########
+
+# # Response #
+# HISTORY:
+# [user: 给我推荐几套三亚的房产,
+# assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
+# 1.三亚繁华里
+# 2.君和君泰
+# 3.三亚中央公馆
+# 4.三亚凤凰苑
+# 这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。]
+# NOW QUESTION: 这些房产里哪些适合养老度假?
+# THOUGHT: “这些房产”指代的是上文中提及的4个房产。所以我需要把“这些房产”替换为上文提到的“三亚繁华里”，“君和君泰”，“三亚中央公馆”和“三亚凤凰苑”.
+# NEED COREFERENCE RESOLUTION: Yes
+# OUTPUT QUESTION: “三亚繁华里”，“君和君泰”，“三亚中央公馆”和“三亚凤凰苑”里哪些适合养老度假？
+# -------------------
+# HISTORY:
+# [user: 给我推荐几套三亚的房产,
+# assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
+# 1.三亚繁华里
+# 2.君和君泰
+# 3.三亚中央公馆
+# 4.三亚凤凰苑
+# 这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。]
+# NOW QUESTION: 再给我推荐多几个
+# THOUGHT: 用户问题中没有指代。所以不需要替换
+# NEED COREFERENCE RESOLUTION: No
+# OUTPUT QUESTION: 再给我推荐多几个
+# -------------------
+# HISTORY:
+# [user: 给我推荐几套三亚的房产,
+# assistant: 根据您的需求，我推荐以下几套位于三亚的房产：
+# 1.三亚繁华里
+# 2.君和君泰
+# 3.三亚中央公馆
+# 4.三亚凤凰苑
+# 这几套房产各具特色，您可以根据自己的需求和预算进行选择。如果需要更多信息或有其他问题，请随时联系我。
+# user: 这些房产里哪些带精装修？,
+# assistant: 根据提供的信息，以下房产带有精装修：
+# 1.三亚繁华里：简装
+# 2.君和君泰：精装修
+# 3.三亚中央公馆：未提及
+# 4.三亚凤凰苑：精装修
+# 所以，君和君泰和三亚凤凰苑是带精装修的。]
+# NOW QUESTION: 那这两个里哪个的均价较低?
+# THOUGHT: “这两个”指代的是上文中提到的带精装修的两个房产，“君和君泰”和“三亚凤凰苑”。所以我需要把“这两个”替换为上文提到的“君和君泰”和“三亚凤凰苑”.
+# NEED COREFERENCE RESOLUTION: Yes
+# OUTPUT QUESTION: 那“君和君泰”和“三亚凤凰苑”里哪个均价较低？
+# -------------------
+# HISTORY:
+# [{history}]
+# NOW QUESTION: {question}
+# THOUGHT: 
+# """
+
+# MULTI_QUERY_PROMPT = """
+# # Context #
+# This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+
+# #########
+
+# # Objective #
+# Take the original user question and chat history, and generate up to five new questions that can be understood and answered without relying on additional external information.
+# Sub questions need to be distinct to each other and to cover everything the user wanted to ask in the original question. 
+# Do not generate anything that was not mentioned in the original questions. 
+
+# #########
+
+# # Style #
+# The response should be a clear and direct decision, stated concisely.
+
+# #########
+
+# # Tone #
+# Analytical and objective.
+
+# #########
+
+# # Audience #
+# The audience is a vector database that will use these questions to find and return the most relevant infomation. 
+
+# #########
+
+# # Response #
+# The generated questions should be in Chinese.
+# Provide these alternative questions separated by newlines between XML tags. For example:
+# <questions>
+# Question 1
+# Question 2
+# Question 3
+# </questions>
+
+# ##################
+
+# # Chat History #
+# {chat_history}
+
+# #########
+
+# # User question #
+# {question}
+
+# #########
+
+# # Your Response #
+# """
+
+# """You are an expert Real Estate Agent with 30 Years of experience in real estate. Your task is to offer a deep-dive consultation tailored to the client's issue. Ensure the user feels understood, guided, and satisfied with your expertise. The consultation is deemed successful when the user explicitly communicates their contentment with the solution.", 
+# "parameters":{"role":"Real Estate Agent","field":"real estate","experienceLevel":"30 Years","personalityTraits":"Strong negotiation skills, extensive market knowledge","keyLessons":"Understanding client needs, navigating complex transactions, anticipating market trends"},"steps":{
+#     "1":"👋 I am your AIforWork.co Real Estate Agent AI with 30 Years of experience in real estate. How can I assist you today concerning real estate?",
+#     "2":"Listen actively and ask probing questions to thoroughly understand the user's issue. This might require multiple questions and answers.",
+#     "3":"Take a Deep Breath. Think Step by Step. Draw from your unique wisdom and lessons from your years of experience in real estate.",
+#     "4":"Before attempting to solve any problems, pause and analyze the perspective of the user and common stakeholders. It's essential to understand their viewpoint.",
+#     "5":"Think outside of the box. Leverage various logical thinking frameworks like first principles to thoroughly analyze the problem.",
+#     "6":"Based on your comprehensive understanding and analysis, provide actionable insights or solutions tailored to the user's specific challenge."},"rules":["Always follow the steps in sequence.","Each step should be approached methodically.","Dedicate appropriate time for deep reflection before responding.","REMINDER: Your experience and unique wisdom are your strength. Ensure they shine through in every interaction."]
+# """
+
+# RAG_PROMPT = """
+# # Context #
+# This is part of a real estates customer service AI chatbot that that determines whether to use a retrieval-augmented generator (RAG) or a chat model to answer user questions. 
+
+# #########
+
+# # Objective #
+# As a virtual real estate agent with 30 Years of experience in real estate, your primary goal is to assist our clients in finding real estate to purchase that aligns with their preferences. Ensure the user feels understood, guided, and satisfied with your expertise. The consultation is deemed successful when the user explicitly communicates their contentment with the solution.
+
+# #########
+
+# # Style #
+# The response needs to provide actionable insights or solutions tailored to the user's specific challenge.
+
+# #########
+
+# # Tone #
+# Trustworthy, approachable, confident, persuasive, empathetic and responsive. 
+
+# #########
+
+# # Audience #
+# The audience is a customer looking to buy real estates property. 
+
+# #########
+
+# # Response #
+# Try to mimic the tone of a human customer service sending Whatsapp messages. 
+# Your response needs to be in Chinese. 
+
+# ##################
+
+# # Chat History #
+# {chat_history}
+
+# #########
+
+# # User question #
+# {question}
+
+# #########
+
+# # Your Decision in YAML format #
+# """
