9b3defd98b817aebf30cf8da287196a6157de9ce
Melo96 <kaiwen961012@gmail.com>
2024-05-24 14:28:08 +0800

    add debug app

---
+++ dev/app_debug.py
@@ -0,0 +1,239 @@
+# # For streamlit cloud deployment
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import os
+import openai
+import cohere
+import json
+import streamlit as st
+import time
+import redis
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.storage import RedisStore
+
+from semantic_router.layer import RouteLayer
+from semantic_router.encoders import OpenAIEncoder
+
+from prompt_template.prompts import *
+from prompt_template.response import *
+from prompt_template.costar_prompts import REPHRASING_DECISION_PROMPT, REPHRASING_PROMPT
+from utils.utils import *
+
+from dotenv import load_dotenv
+load_dotenv()
+
+rerank = False
+router_type = 'llm'
+
+llm = "gpt-4o"
+reranker = 'rerank-multilingual-v3.0'
+collection_name = "summaries"
+persist_directory = "data/chroma_openai"
+redis_host = os.environ['REDIS_HOST']
+redis_port = '10020'
+redis_password = os.environ['REDIS_PASSWORD']
+top_k = 10
+doc_id_key = "doc_id"
+
+def chat_llm(user_input, system_prompt='', chat_history=[], temperature=0.2):
+    messages = [{"role": 'system', "content": system_prompt}] if system_prompt else []
+    if chat_history:
+        messages+=chat_history
+    messages += [{'role': 'user', 'content': user_input}]
+
+    response = st.session_state['llm_client'].chat.completions.create(
+            model=llm,
+            messages=messages,
+            temperature=temperature
+        )
+    message = response.choices[0].message.content
+    return message
+
+@st.cache_resource
+def initialize_chain():
+    # The vectorstore to use to index the child chunks
+    vectorstore = Chroma(
+        collection_name=collection_name,
+        embedding_function=OpenAIEmbeddings(),
+        persist_directory=persist_directory,
+    )
+    # The storage layer for the parent documents
+    redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+    docstore = RedisStore(client=redis_client)
+
+    # Load models
+    encoder = OpenAIEncoder()
+    reranker = cohere.Client(os.environ['COHERE_API_KEY'])
+    llm_client = openai.Client()
+
+    # Load routers
+    routers = get_routes()
+    return [vectorstore, docstore, llm_client, encoder, reranker, routers]
+
+def coreference_resolution(ori_query):
+    print(f'Coreference resolution: {ori_query}')
+    if st.session_state['messages']:
+        user_input_history = '[' + ', '.join(
+                                f"user: {item['content']}\n" if item['role'] == 'user' else f"assistant: {item['content']}\n"
+                                for item in st.session_state['messages']
+                            ) + ']'
+
+        output = chat_llm(COREFERENCE_RESOLUTION.format(history=user_input_history,
+                                                        question=ori_query),
+                          temperature=0
+                          )
+        ori_query = output_parser(output, 'OUTPUT QUESTION: ')
+    return ori_query
+
+def multi_queries_retrieval(ori_query):
+    # Multi-query generation
+    s = time.time()
+    multi_query = chat_llm(ori_query, MULTI_QUERY_PROMPT, st.session_state['messages'], temperature=0)
+    e = time.time()
+    print(f'Multi queries: {e-s} seconds')
+    queries = [ori_query] + multi_query.split("\n")[1:-1]
+
+    # Get the matches for each query
+    with ThreadPoolExecutor(max_workers=max(6, os.cpu_count()//2)) as executor:
+        vectore_search_partial = partial(st.session_state['vectorstore'].similarity_search_with_relevance_scores, 
+                                        k=top_k, 
+                                        score_threshold=0.7)
+        nested_results = executor.map(vectore_search_partial, queries)
+
+    matches = [item for sub_list in nested_results for item in sub_list]
+    match_list_text = ''
+    if matches:
+        doc_ids = set(map(lambda d: d[0].metadata['doc_id'], matches))
+
+        matches = st.session_state['docstore'].mget(list(doc_ids))
+        match_list = [json.loads(match) for match in matches]
+        match_list_text = [match['page_content'] for match in match_list]
+    return match_list_text
+
+def query_rephrase(query):
+    decision = chat_llm(REPHRASING_DECISION_PROMPT.format(chat_history=st.session_state['history'], question=query))
+    if 'true' in decision:
+        query = chat_llm(REPHRASING_PROMPT.format(chat_history=st.session_state['history'], question=query))
+    return query
+
+@st.spinner('正在打字...')
+def rag(ori_query):
+    if st.session_state['messages']:
+        st.session_state['history'] = json.dumps(st.session_state['messages'], ensure_ascii=False)
+    # Query Rephrasing
+    s = time.time()
+    ori_query = query_rephrase(ori_query)
+    e = time.time()
+    print(f'Query rephrasing: {ori_query}, {e-s} seconds')
+
+    # Coreference Resolution
+    s = time.time()
+    ori_query = coreference_resolution(ori_query)
+    e = time.time()
+    print(f'Coreference resolution: {ori_query}, {e-s} seconds')
+
+    # RAG Router
+    s = time.time()
+    router_result = chat_llm(RAG_ROUTER_PROMPT.format(question=ori_query), chat_history=st.session_state['messages'], temperature=0)
+    e = time.time()
+    print(f"RAG Router: {router_result}, {e-s} seconds")
+
+    result_text = ''
+    if 'no' in router_result:
+        # Multi-query
+        s = time.time()
+        match_list_text = multi_queries_retrieval(ori_query)
+        e = time.time()
+        print(f'Vector search: {e-s} seconds')
+
+        if not match_list_text:
+            return ''
+
+        # Reranking
+        if rerank:
+            s = time.time()
+            results = st.session_state['reranker'].rerank(model=reranker, query=ori_query, documents=match_list_text, top_n=20, return_documents=True)
+            result_text_list = [item.document.text for item in results.results]
+            result_text = ''.join(f'<context>{t}</context>' for t in result_text_list)
+            e = time.time()
+            print(f'Reranking: {e-s} seconds')
+        else:
+            result_text = ''.join(f'<context>{t}</context>' for t in match_list_text)
+
+    # Response
+    response = chat_llm(RAG_USER_PROMPT.format(ori_query, result_text), RAG_SYSTEM_PROMPT, st.session_state['messages'])
+    return response
+
+def chat(ori_query):
+    # Query Router
+    s = time.time()
+    router_result = chat_llm(QUERY_ROUTER_PROMPT.format(question=ori_query), chat_history=st.session_state['messages'], temperature=0)
+    e = time.time()
+    print(f"Query Router: {router_result}, {e-s} seconds")
+
+    # Call RAG or directly call LLM
+    s = time.time()
+    if 'query' in router_result:
+        response = rag(ori_query)
+        st.session_state['messages'].append({"role": "user", "content": ori_query})
+        st.session_state['messages'].append({"role": "assistant", "content": response})
+    else:
+        response = chat_llm(ori_query, CHAT_SYSTEM_PROMPT, chat_history=st.session_state['messages'])
+    e = time.time()
+    print(f'Response: {e-s} seconds')
+
+    if response:
+        with st.chat_message("assistant"):
+            st.markdown(f"""
+                {str(response)}
+                <br />
+                <br />
+            """,
+            unsafe_allow_html=True
+            )
+    else:
+        with st.chat_message("assistant"):
+            st.write(NO_RELEVANT_FILES)
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.text_input(label="姓名")
+        with col2:
+            st.text_input(label="手机号")
+    return response
+
+st.title("爱房网智能客服DEMO")
+with st.chat_message("assistant"):
+    st.write('您好！感谢您选择爱房网。我是您的专属房产咨询助手小盖。请问有什么可以帮助您的吗？')
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state['display_messages'] = []
+    st.session_state['messages'] = []
+    st.session_state['history'] = ''
+
+# Display chat messages from history on app rerun
+for message in st.session_state['display_messages']:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+sesstion_state_name = ['vectorstore', 'docstore', 'llm_client', 'encoder', 'reranker', 'routers']
+init = initialize_chain()
+for name, func in zip(sesstion_state_name, init):
+    st.session_state[name] = func
+
+if prompt := st.chat_input('请在这里输入消息，点击Enter发送'):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    response = chat(prompt)
+
+    st.session_state['display_messages'].append({"role": "user", "content": prompt})
+    st.session_state['display_messages'].append({"role": "assistant", "content": response})
