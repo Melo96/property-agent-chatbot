717205f4a35b29c0b58a0c166220e9d4c8cba1cb
Melo96 <kaiwen961012@gmail.com>
2024-06-03 16:21:18 +0800

    streamlit sharing deployment

--- dev/app.py
+++ dev/app.py
@@ -1,7 +1,7 @@
 # For streamlit cloud deployment
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
